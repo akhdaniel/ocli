9e26ea96e789cada5bb62ffee40603955bbd3712
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-09-22 20:14:53 +0700

    Fix ESLint errors in DateField and DatetimeField components - Import computed from vue to fix 'computed is not defined' errors - Ensure proper ESLint compliance for all field components

--- src/components/fields/DateField.vue
+++ src/components/fields/DateField.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+import { computed } from 'vue'
+
 export default {
   name: 'DateField',
   props: {
--- src/components/fields/DatetimeField.vue
+++ src/components/fields/DatetimeField.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+import { computed } from 'vue'
+
 export default {
   name: 'DatetimeField',
   props: {
